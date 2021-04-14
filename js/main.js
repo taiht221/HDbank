4421137853cab047e53f8991c69dea20aae3fc37
Huynh Tien Tai <taiht221@gmail.com>
2021-04-14 22:25:24 +0700

    new page

--- js/main.js
+++ js/main.js
@@ -38,3 +38,33 @@ function stopPropagation(e) {
 $(window).on("load", function () {
   $("#preloder").fadeOut(2000);
 });
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("open-modal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+modal.onclick = function () {
+  modal.style.display = "none";
+};
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Escape") {
+    modal.style.display = "none";
+  }
+});
+$(".product-img").click(function (event) {
+  event.stopPropagation();
+  // Do something
+});
