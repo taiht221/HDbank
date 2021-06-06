59119438ceb4ddb15501ba1df83a5630222df54b
Huynh Tien Tai <taiht221@gmail.com>
2021-06-06 15:01:59 +0700

    fix

--- js/main.js
+++ js/main.js
@@ -1,78 +1,64 @@
 // header change color
 
 $(document).ready(function () {
-  var count_particles, stats, update;
-  stats = new Stats();
-  stats.setMode(0);
-  stats.domElement.style.position = "absolute";
-  stats.domElement.style.left = "0px";
-  stats.domElement.style.top = "0px";
-  document.body.appendChild(stats.domElement);
-  count_particles = document.querySelector(".js-count-particles");
+  var count_particles, update
+  count_particles = document.querySelector('.js-count-particles')
   update = function () {
-    stats.begin();
-    stats.end();
-    if (
-      window.pJSDom[0].pJS.particles &&
-      window.pJSDom[0].pJS.particles.array
-    ) {
-      count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
-    }
-    requestAnimationFrame(update);
-  };
-  requestAnimationFrame(update);
-});
+    requestAnimationFrame(update)
+  }
+  requestAnimationFrame(update)
+})
 // todo
 $(document).ready(function () {
   function menuTop() {
-    if (window.location.pathname === "/") {
+    if (window.location.pathname === '/' && window.location.search === '') {
       if ($(this).scrollTop() >= 200) {
-        $(".header").addClass("active");
+        $('.header').addClass('active')
       } else {
-        $(".header").removeClass("active");
+        $('.header').removeClass('active')
       }
     } else {
-      $(".header").addClass("active");
+      $('.header').addClass('active')
     }
   }
-  menuTop();
-  $(window).scroll(menuTop);
-  $(".hamburger").on("click", function () {
-    $(".hamburger").toggleClass("active");
-    $(".menu-right").toggleClass("active");
-  });
-});
+  menuTop()
+  $(window).scroll(menuTop)
+  $('.hamburger').on('click', function () {
+    $('.hamburger').toggleClass('active')
+    $('.menu-right').toggleClass('active')
+  })
+})
 // loading screen
-$(window).on("load", function () {
-  $("#preloder").fadeOut(2000);
-});
+$(window).on('load', function () {
+  $('#preloder').fadeOut(2000)
+})
 // Get the modal
-var modal = document.getElementById("myModal");
+var modal = document.getElementById('myModal') || ''
 
 // Get the button that opens the modal
-var btn = document.getElementById("open-modal");
+var btn = document.getElementById('open-modal') || ''
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName('close')[0] || ''
 
 // When the user clicks the button, open the modal
 btn.onclick = function () {
-  modal.style.display = "block";
-};
+  modal.style.display = 'block'
+}
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
-  modal.style.display = "none";
-};
+  modal.style.display = 'none'
+}
 modal.onclick = function () {
-  modal.style.display = "none";
-};
-document.addEventListener("keydown", function (event) {
-  if (event.key === "Escape") {
-    modal.style.display = "none";
+  modal.style.display = 'none'
+}
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Escape') {
+    modal.style.display = 'none'
   }
-});
-$(".product-img").click(function (event) {
-  event.stopPropagation();
+})
+$('.product-img').click(function (event) {
+  event.stopPropagation()
   // Do something
-});
+})
